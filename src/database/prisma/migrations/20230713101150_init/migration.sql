9b4d62f30e16d95f51fe2a55377a2955ddadfdbf
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-13 13:19:23 +0300

    Add user's migrations

---
+++ src/database/prisma/migrations/20230713101150_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT,
+    "birthdate" TIMESTAMP(3) NOT NULL,
+    "likes" TEXT,
+    "favoriteClubs" TEXT,
+    "preferredFoods" TEXT,
+    "hobbies" TEXT,
+    "Role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
