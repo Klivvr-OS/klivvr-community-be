a1d8f73389bb3bfeade197af4d3593eb75693557
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-17 17:22:42 +0300

    convert style to async await

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,28 +1,26 @@
 import express, { type Request, type Response } from 'express';
 import { userService } from '../modules';
-import { type User } from '../modules/User/types/register';
+import { type User as UserType } from '@prisma/client';
 
 const router = express.Router();
 
-router.post('/register', (req: Request, res: Response): any => {
-  userService
-    .createOne(req)
-    .then((user: User) => {
-      return res.status(201).json({
-        message: 'User created successfully',
-        userData: {
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          isVerified: user.isVerified,
-        },
-      });
-    })
-    .catch((e: any) => {
-      return res.status(500).json({
-        message: e?.message ?? 'Internal Server Error',
-      });
+router.post('/register', async (req: Request, res: Response) => {
+  try {
+    const user: UserType = await userService.createOne(req);
+    return res.status(201).json({
+      message: 'User created successfully',
+      userData: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        isVerified: user.isVerified,
+      },
     });
+  } catch (e: any) {
+    return res.status(500).json({
+      message: e?.message ?? 'Internal Server Error',
+    });
+  }
 });
 
 export default router;
