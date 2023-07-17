242496312bcb7178dcac9e2d11998fba6361f4d8
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-17 11:41:39 +0300

    feat: sending emails

---
+++ src/mailers/sendEmail.ts
@@ -0,0 +1,28 @@
+import sendGrid from '@sendgrid/mail';
+import { sendGridAPIKey, sendGridFromEmail } from '../config';
+import { type sendVerificationEmail as sendGridType } from '../mailers/types/sendEmail';
+
+sendGrid.setApiKey(sendGridAPIKey);
+
+export async function sendEmail(
+  to: string,
+  subject: string,
+  text: string,
+  html: string,
+): Promise<void> {
+  const msg: sendGridType = {
+    to,
+    from: sendGridFromEmail,
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    await sendGrid.send(msg);
+  } catch (err: any) {
+    throw new Error(`Error sending email`);
+  }
+}
+
+export default sendEmail;
