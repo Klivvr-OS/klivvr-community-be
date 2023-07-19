071fafd52a2a91048904265395a402bc34d6d36c
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-19 19:09:15 +0300

    modify text in send email, and return created user

--- src/modules/User/services/userService.ts
+++ src/modules/User/services/userService.ts
@@ -2,7 +2,7 @@ import { userRepo, type UserRepo } from '../repos/userRepo';
 import { Prisma } from '@prisma/client';
 import sendGridEmail from '../../../mailers/sendEmail';
 import { generateVerificationCode } from '../../../helpers/verificationCode';
-import { ErrorHandler } from '../../../middlewares';
+import { CustomError } from '../../../middlewares';
 
 export class UserService {
   constructor(private readonly userRepo: UserRepo) {}
@@ -12,7 +12,7 @@ export class UserService {
       email: args.email,
     });
     if (existingUser != null) {
-      throw new ErrorHandler('User already exists', 409);
+      throw new CustomError('User already exists', 409);
     }
     const code = generateVerificationCode();
     const createdUser = await this.userRepo.createOne({
@@ -22,11 +22,10 @@ export class UserService {
     await sendGridEmail(
       createdUser.email,
       'Welcome to Klivvr',
-      'Welcome to Klivvr',
-      `<h1>Welcome to Klivvr</h1><p>Thank you for registering with Klivvr. Here is your verification code ${code},
-        `,
+      `Welcome to Klivvr Thank you for registering with Klivvr. Here is your verification code ${code}`,
+      `<h1>Welcome to Klivvr</h1><p>Thank you for registering with Klivvr. Here is your verification code ${code}</p>`,
     );
-    return createdUser != null;
+    return createdUser;
   }
 
   async findOne(args: Prisma.UserWhereInput) {
