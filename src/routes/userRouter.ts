d9025a31519af6c57828442f233a9be40e9a2dc3
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-19 14:21:50 +0300

    modify response, and add wrapper function

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,26 +1,23 @@
-import express, { type Request, type Response } from 'express';
+import express, { Request, Response } from 'express';
 import { userService } from '../modules';
-import { type User as UserType } from '@prisma/client';
+import { Prisma } from '@prisma/client';
+import { endpoint } from '../core/endpoint';
 
 const router = express.Router();
 
-router.post('/register', async (req: Request, res: Response) => {
-  try {
-    const user: UserType = await userService.createOne(req);
-    return res.status(201).json({
-      message: 'User created successfully',
-      userData: {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        isVerified: user.isVerified,
-      },
-    });
-  } catch (e: any) {
-    return res.status(e?.status ?? 500).json({
-      message: e?.message ?? 'Internal Server Error',
-    });
-  }
-});
+router.post(
+  '/register',
+  endpoint(async (req: Request, res: Response) => {
+    const user = await userService.createOne(
+      req.body as Prisma.UserUncheckedCreateInput,
+    );
+    if (user != null) {
+      res.status(201).json({
+        message:
+          'User created successfully, Please check your email for verification code',
+      });
+    }
+  }),
+);
 
 export default router;
