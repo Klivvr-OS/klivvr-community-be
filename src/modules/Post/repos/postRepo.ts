4a3c917669b8fcc71f63a086d9e7088dcdbc250e
johnnabil <john.nabil10@yahoo.com>
2023-07-19 12:17:07 +0300

    Refactor PostRepo to use instantiated PrismaClient
    - Replaced the imported prisma instance with a locally instantiated PrismaClient

--- src/modules/Post/repos/postRepo.ts
+++ src/modules/Post/repos/postRepo.ts
@@ -2,32 +2,32 @@ import { Prisma, type PrismaClient } from '@prisma/client';
 import prisma from '../../../database/prisma/client';
 
 export class PostRepo {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly client: PrismaClient) {}
 
   async createOne(args: Prisma.PostUncheckedCreateInput) {
-    return await prisma.post.create({
+    return await this.client.post.create({
       data: args,
     });
   }
 
   async findMany() {
-    return await prisma.post.findMany();
+    return await this.client.post.findMany();
   }
 
   async findOne(query: Prisma.PostWhereUniqueInput) {
-    return await prisma.post.findFirst({
+    return await this.client.post.findFirst({
       where: query,
     });
   }
 
   async updateOne(query: Prisma.PostWhereUniqueInput, args: Prisma.PostUpdateInput) {
-    return await prisma.post.update({
+    return await this.client.post.update({
       where: query,
       data: args,
     });
   }
   async deleteOne(query: Prisma.PostWhereUniqueInput) {
-    return await prisma.post.delete({
+    return await this.client.post.delete({
       where: query,
     });
   }
