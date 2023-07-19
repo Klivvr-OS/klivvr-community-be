e2c0475fc590783ceff31eb2ebf87f4820471ac4
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-19 14:20:22 +0300

    refactor types for functions

--- src/modules/User/repos/userRepo.ts
+++ src/modules/User/repos/userRepo.ts
@@ -1,42 +1,28 @@
-import { type PrismaClient, type User as UserType } from '@prisma/client';
+import {
+  Prisma,
+  type PrismaClient,
+  type User as UserType,
+} from '@prisma/client';
 import prisma from '../../../database/client';
-import { type User } from '../types/register';
 
 export class UserRepo {
   constructor(private readonly prisma: PrismaClient) {}
 
-  async createOne({
-    firstName,
-    lastName,
-    email,
-    password,
-  }: User): Promise<UserType> {
-    return await this.prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password,
-      },
-    });
+  async createOne(args: Prisma.UserUncheckedCreateInput) {
+    return await this.prisma.user.create({ data: args });
   }
 
-  async findOneByEmail(email: string): Promise<UserType | null> {
-    return await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+  async findOne(query: Prisma.UserWhereInput) {
+    return await this.prisma.user.findFirst({ where: query });
   }
 
-  async updateOne(email: string, verificationCode: string): Promise<UserType> {
+  async updateOne(
+    query: Prisma.UserWhereUniqueInput,
+    args: Prisma.UserUncheckedUpdateInput,
+  ): Promise<UserType> {
     return await this.prisma.user.update({
-      where: {
-        email,
-      },
-      data: {
-        verificationCode,
-      },
+      where: query,
+      data: args,
     });
   }
 }
