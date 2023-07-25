fceceeb23793079625d8d1aa7494b302d20db1c0
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-25 14:06:52 +0300

    add cloudinary env variables

---
+++ src/config/cloudinary.env.ts
@@ -0,0 +1,5 @@
+import env from './env';
+
+export const cloudinaryCloudName = env('CLOUDINARY_CLOUD_NAME');
+export const cloudinaryAPIKey = env('CLOUDINARY_API_KEY');
+export const cloudinaryAPISecret = env('CLOUDINARY_API_SECRET');
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export { default as env } from './env';
 export * from './sendgrid.env';
+export * from './cloudinary.env';
