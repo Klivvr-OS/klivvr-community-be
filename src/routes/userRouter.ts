268d5672457626a935cc3b01d28f8c38dc9b17b1
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-26 16:24:36 +0300

    feat: Enable image upload during user registration and add input parsing for validation
    Uploaded image to cloudinary while registration process and added validation for inputs

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,14 +1,33 @@
 import express from 'express';
 import { userService } from '../modules';
-import { Prisma } from '@prisma/client';
 import { endpoint } from '../core/endpoint';
+import { multerUpload } from '../middlewares';
+import { cloudinaryInstance } from '../modules/Cloudinary/services/Cloudinary';
+import { handleMulterError } from '../middlewares';
 
 const router = express.Router();
 
 router.post(
   '/register',
+  multerUpload.single('image'),
+  handleMulterError,
   endpoint(async (req, res) => {
-    await userService.createOne(req.body as Prisma.UserUncheckedCreateInput);
+    let photoURL;
+    if (req.file) {
+      const localFilePath = req.file.path;
+      const { isSuccess, imageURL } = await cloudinaryInstance.uploadImage(
+        localFilePath,
+      );
+      if (!isSuccess) {
+        throw new Error();
+      }
+      photoURL = imageURL;
+    }
+    const userScehma = userService.createUserSchema.parse({
+      ...req.body,
+      photoURL,
+    });
+    await userService.createOne(userScehma);
     res.status(201).json({
       message:
         'User created successfully, Please check your email for verification code',
