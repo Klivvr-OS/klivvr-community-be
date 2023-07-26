d0129747688a4ef5d3d494fb355b19f2be8d01d6
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-26 16:05:40 +0300

    feat: Add sendGrid config variables
    Added three variables to the configuration file to store SendGrid settings.

--- src/config/sendgrid.env.ts
+++ src/config/sendgrid.env.ts
@@ -2,3 +2,6 @@ import env from './env';
 
 export const sendGridAPIKey = env('SENDGRID_API_KEY');
 export const sendGridFromEmail = env('SENDGRID_FROM_EMAIL');
+export const sendGridSubject = env('SENDGRID_SUBJECT');
+export const sendGridText = env('SENDGRID_TEXT');
+export const sendGridHTML = env('SENDGRID_HTML');
