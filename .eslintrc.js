c721e2c6e8b24fd7dbcb33549331f2839c08b4bd
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-30 16:17:05 +0300

    changed regex from I[A-Z] to [A-Z] to match custom.d.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
         selector: 'interface',
         format: ['PascalCase'],
         custom: {
-          regex: '^I[A-Z]',
+          regex: '^[A-Z]',
           match: true,
         },
       },
