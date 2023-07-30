731c6e91056d3bfd7ac5492b3fdcac4e44770f8b
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-30 16:11:35 +0300

    modify is-auth middleware
    stored user property in req.user instead of req.locals.user

--- src/middlewares/is-auth.ts
+++ src/middlewares/is-auth.ts
@@ -12,7 +12,7 @@ export const isAuth = async (
     const token = req.cookies.accessToken;
     const user = await userService.authenticateUser(token, secretAccessKey);
     if (user) {
-      res.locals.user = user;
+      req.user = user;
       next();
     } else {
       throw new CustomError('Unauthorized', 401);
