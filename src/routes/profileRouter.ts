91aebd093a3905a915bf8c757e28d36480026ce6
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-08-02 12:26:56 +0300

    add new endpoint
    in this commit, I made a new router called profileRouter that points to /profile and add put http request with endpoint /me ro udapte user data

---
+++ src/routes/profileRouter.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import { userService } from '../modules';
+import { endpoint } from '../core/endpoint';
+import { multerUpload, handleMulterError, isAuth } from '../middlewares';
+import { cloudinaryInstance } from '../modules/Cloudinary/services/Cloudinary';
+
+const router = express.Router();
+
+router.put(
+  '/me',
+  isAuth,
+  multerUpload.single('avatar'),
+  handleMulterError,
+  endpoint(async (req, res) => {
+    const userId = req.user?.id;
+    let photoURL;
+    if (req.file) {
+      const localFilePath = req.file.path;
+      const { isSuccess, imageURL } = await cloudinaryInstance.uploadImage(
+        localFilePath,
+      );
+      if (!isSuccess) {
+        throw new Error();
+      }
+      photoURL = imageURL;
+    }
+    const updateUserSchema = userService.validateUpdateUserSchema.parse(
+      req.body,
+    );
+    if (photoURL) {
+      updateUserSchema.photoURL = photoURL;
+    }
+    await userService.updateOne({ id: userId }, updateUserSchema);
+    res
+      .status(200)
+      .json({ message: 'Updated data successfully', data: updateUserSchema });
+  }),
+);
+
+export default router;
