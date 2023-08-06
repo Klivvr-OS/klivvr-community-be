cc8bc9f1509109b497f0e4ca1fab11925a4173e3
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-08-06 18:27:21 +0300

    modify /posts endpoint Co-authored-by: johnnabil <john.nabil10@yahoo.com>
    in this commit, modified /posts endpoint, to fit with changes that we did (pagination and queries), aslo added validation for req.query
    
    Signed-off-by: MuhammedMagdyy <mohamedmody04@gmail.comm>

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -7,6 +7,7 @@ import { Request, Response } from 'express';
 import { endpoint } from '../core/endpoint';
 import { CustomError } from '../middlewares';
 import { isAuth } from '../middlewares/';
+import { validateRequestQuery } from '../helpers';
 
 const router = express.Router();
 
@@ -42,7 +43,11 @@ router.get(
   '/',
   isAuth,
   endpoint(async (req, res) => {
-    const postObjects = await postService.findMany();
+    const { pageNumber, pageSize } = validateRequestQuery.parse(req.query);
+    const postObjects = await postService.findMany(
+      {},
+      { pageNumber, pageSize },
+    );
     if (!postObjects) {
       throw new CustomError('Posts not found', 404);
     }
