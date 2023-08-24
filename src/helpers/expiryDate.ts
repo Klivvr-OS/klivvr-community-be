6378fa0711dbc63a9ae391d10485577ce6579a10
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-08-24 16:26:29 +0300

    user timstring package Co-authored-by: johnnabil <john.nabil10@yahoo.com>

--- src/helpers/expiryDate.ts
+++ src/helpers/expiryDate.ts
@@ -1,14 +1,9 @@
 import { CustomError } from '../middlewares';
+import timestring from 'timestring';
 
 export function expiryDate(expiry: string) {
-  const numericExpiry = parseInt(expiry);
-  const minute = 60 * 1000;
-  const week = 7 * 24 * 60 * 60 * 1000;
-  if (expiry.endsWith('m')) {
-    return numericExpiry * minute;
-  } else if (expiry.endsWith('w')) {
-    return numericExpiry * week;
-  } else {
+  if (!expiry) {
     throw new CustomError('Invalid expiry date', 400);
   }
+  return timestring(expiry, 'ms');
 }
