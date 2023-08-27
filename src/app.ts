a9f9a9fbf002f8d7b1961af47cfbcde3cf6ecc6d
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-08-27 17:29:18 +0300

    new enhacments
    Get tokens from authorization header.
    removed cookies.
    removed logout endpoint.
    returned access token and its expiry date in refresh endpoint.
    Co-authored-by: johnnabil <john.nabil10@yahoo.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import routes from './routes';
 import { errorHandlerMiddleware } from './middlewares/';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(routes);
 app.use(errorHandlerMiddleware);
 
