ac581b71c71ebab883a2711e9d89ce2fb0022290
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-08-29 13:46:37 +0300

    notifications endpoints
    - Created a new endpoint called notifications to create a notification.
    - Feteched all user notifications by his id.

---
+++ src/routes/notificationRouter.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { endpoint } from '../core/endpoint';
+import { notificationService } from '../modules';
+import { requestQueryPaginationSchema } from '../helpers';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  endpoint(async (req, res) => {
+    const notification = notificationService.createNotificationSchema.parse(
+      req.body,
+    );
+    const userId = req.user?.id;
+    await notificationService.createOne({
+      ...notification,
+      userId: userId as number,
+    });
+    res.status(201).json({ message: 'Notification created successfully' });
+  }),
+);
+
+router.get(
+  '/:id',
+  endpoint(async (req, res) => {
+    const userId = Number(req.params.id);
+    const { pageNumber, pageSize } = requestQueryPaginationSchema.parse(
+      req.query,
+    );
+    const notifications = await notificationService.findMany(
+      {
+        userId,
+      },
+      { pageNumber, pageSize },
+    );
+    if (!notifications) {
+      res.status(404).json({ message: 'Notifications not found' });
+    }
+    res.status(200).json({ notifications });
+  }),
+);
+
+export default router;
