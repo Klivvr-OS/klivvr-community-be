c2d510dc4b97d19081262157266db7cb4255fbd6
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-08-29 14:39:22 +0300

    enhacements
    - removed (s) from sendNotifications function.
    - added args object to parameter.

--- src/modules/PushNotification/services/pushNotificationService.ts
+++ src/modules/PushNotification/services/pushNotificationService.ts
@@ -5,19 +5,20 @@ admin.initializeApp({
   credential: admin.credential.cert(firebasePrivateKey),
 });
 
-export async function sendNotifications(
-  deviceToken: string,
-  title: string,
-  description: string,
-) {
-  const message = {
-    notification: {
-      title,
-      body: description,
-    },
-    token: deviceToken,
-  };
+export async function sendNotification(args: {
+  deviceToken: string;
+  title: string;
+  description: string;
+}) {
   try {
+    const { deviceToken, title, description } = args;
+    const message = {
+      notification: {
+        title,
+        body: description,
+      },
+      token: deviceToken,
+    };
     await admin.messaging().send(message);
   } catch (err) {
     throw new Error();
