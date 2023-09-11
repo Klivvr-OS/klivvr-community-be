296eff07fe5d98c0e692fbd3d262f518ba59db44
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-11 16:19:46 +0300

    changed imports
    - replace pushNotifications to novuService

--- src/helpers/jobs.ts
+++ src/helpers/jobs.ts
@@ -1,10 +1,10 @@
 import cron from 'node-cron';
-import { notificationService, pushNotificationService } from '../modules';
+import { notificationService, novuService } from '../modules';
 
 export function eventsNotificationsEveryDay() {
   cron.schedule('0 11 * * *', async () => {
     // send notifications at 12:00 PM every day
-    await pushNotificationService.registerUsersToNovu();
+    await novuService.registerUsersToNovu();
     await notificationService.sendEventsNotifications();
   });
 }
