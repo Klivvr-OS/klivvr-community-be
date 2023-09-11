6bcc208451c49f348029833b6bfe97d45f75dac3
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-11 16:18:24 +0300

    Refactor
    - refactored sendEventNotifications by reduce lines also merge birthday and anniversary together.
    - fixed some imports and moved functions to their module

--- src/modules/Notification/services/notificationService.ts
+++ src/modules/Notification/services/notificationService.ts
@@ -1,8 +1,6 @@
 import { Prisma, Event } from '@prisma/client';
 import { NotificationRepo, notificationRepo } from '../repos/notificationRepo';
-import { pushNotificationService } from '../../PushNotification';
-import { eventService } from '../../Event';
-import { userService } from '../../User';
+import { eventService, userService, novuService } from '../../../modules';
 
 export class NotificationService {
   constructor(private readonly notificationRepo: NotificationRepo) {}
@@ -11,83 +9,73 @@ export class NotificationService {
     return await this.notificationRepo.createOne(args);
   }
 
-  isEventToday(eventDate: Date) {
-    const today = new Date();
-    return (
-      eventDate.getDate() === today.getDate() &&
-      eventDate.getMonth() === today.getMonth()
-    );
-  }
-
-  convertEventStringsToTitleAndType(eventName: string, eventType: string) {
-    const eventNameParts = eventName.split(' ');
-    const firstTwoWords = eventNameParts.splice(0, 2).join(' ');
-    const capitalizedEventType =
-      eventType.charAt(0).toUpperCase() + eventType.slice(1).toLowerCase();
-    return { eventTitle: firstTwoWords, newType: capitalizedEventType };
-  }
-
   async sendEventsNotifications() {
     const events = (await eventService.findThisWeekEvents({
       pageNumber: 1,
       pageSize: 10,
-    })) as Array<Event>;
+    })) as Event[];
+
     const todayEvents = events.filter((event) => {
-      const eventDate = new Date(event.date);
-      return this.isEventToday(eventDate);
+      return eventService.isEventToday(new Date(event.date));
     });
+
     const users = await userService.findUsersDeviceToken();
-    todayEvents.forEach((event: Event) => {
+
+    todayEvents.forEach((event) => {
       users.forEach(async (user) => {
         let title = '',
           description = '';
-        if (user.id === event.userId && event.eventType === 'BIRTHDAY') {
-          title = `Today is your Birthday 🎉`;
-          description = `Happy birthday to you!`;
-        } else if (
-          user.id === event.userId &&
+        if (
+          (user.id === event.userId && event.eventType === 'BIRTHDAY') ||
+          (user.id === event.userId && event.eventType === 'ANNIVERSARY')
+        ) {
+          title = `Today is your ${event.eventType.toLowerCase()} 🎉`;
+          description = `Happy ${event.eventType.toLowerCase()} ${
+            eventService.convertEventStringsToTitleAndType(
+              event.name,
+              event.eventType,
+            ).eventTitle
+          }!`;
+        } else if (user.id === event.userId) return;
+        else if (
+          event.eventType === 'BIRTHDAY' ||
           event.eventType === 'ANNIVERSARY'
         ) {
-          title = `Today is your Anniversary 🎉`;
-          description = `Happy anniversary to you!`;
-        } else if (user.id === event.userId) return;
-        else if (event.eventType === 'BIRTHDAY') {
           title = `Today is ${event.name} 🎉`;
           description = `Don't miss to wish ${
-            this.convertEventStringsToTitleAndType(event.name, event.eventType)
-              .eventTitle
+            eventService.convertEventStringsToTitleAndType(
+              event.name,
+              event.eventType,
+            ).eventTitle
           } a happy ${
-            this.convertEventStringsToTitleAndType(event.name, event.eventType)
-              .newType
-          }!`;
-        } else if (event.eventType === 'ANNIVERSARY') {
-          title = `Today is ${event.name} 🎉`;
-          description = `Don't miss to wish ${
-            this.convertEventStringsToTitleAndType(event.name, event.eventType)
-              .eventTitle
-          } a happy ${
-            this.convertEventStringsToTitleAndType(event.name, event.eventType)
-              .newType
+            eventService.convertEventStringsToTitleAndType(
+              event.name,
+              event.eventType,
+            ).newType
           }!`;
         } else if (event.eventType === 'WEDDING') {
           title = `Today is ${event.name} 🤵👰`;
           description = `Don't miss to wish ${
-            this.convertEventStringsToTitleAndType(event.name, event.eventType)
-              .eventTitle
+            eventService.convertEventStringsToTitleAndType(
+              event.name,
+              event.eventType,
+            ).eventTitle
           } a lifetime of love and happiness!`;
         } else if (event.eventType === 'FAREWELL') {
           title = `Today is ${event.name} 😥`;
           description = `Don't miss to wish ${
-            this.convertEventStringsToTitleAndType(event.name, event.eventType)
-              .eventTitle
+            eventService.convertEventStringsToTitleAndType(
+              event.name,
+              event.eventType,
+            ).eventTitle
           } luck in his new journey!`;
         }
         await Promise.all([
-          pushNotificationService.notificationsTrigger(
+          novuService.notificationsTrigger(
             { title, description },
             user.id.toString(),
           ),
-          this.notificationRepo.createOne({
+          this.createOne({
             title,
             description,
             userId: user.id,
