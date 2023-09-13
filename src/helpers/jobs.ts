dd1a6e57aab7366b90e27ba9dbdd0d90d53b9f06
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-13 17:26:16 +0300

    Modify: delete novuService import

--- src/helpers/jobs.ts
+++ src/helpers/jobs.ts
@@ -1,10 +1,9 @@
 import cron from 'node-cron';
-import { notificationService, novuService } from '../modules';
+import { notificationService } from '../modules';
 
 export function eventsNotificationsEveryDay() {
   cron.schedule('0 11 * * *', async () => {
     // send notifications at 12:00 PM every day
-    await novuService.registerUsersToNovu();
     await notificationService.sendEventsNotifications();
   });
 }
