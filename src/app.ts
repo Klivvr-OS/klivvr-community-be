cfbbd906ed0eb6ff4cf6e5a81bdd2872ed550900
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-14 16:27:30 +0300

    Refactor: cron jobs
    - Chagned function name to follow naming convention.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import { errorHandlerMiddleware } from './middlewares/';
-import { eventsNotificationsEveryDay } from './helpers';
+import { scheduleDailyNotification } from './helpers';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use(errorHandlerMiddleware);
-eventsNotificationsEveryDay();
+scheduleDailyNotification();
 
 export default app;
--- src/helpers/jobs.ts
+++ src/helpers/jobs.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { notificationService } from '../modules';
 
-export function eventsNotificationsEveryDay() {
+export function scheduleDailyNotification() {
   cron.schedule('0 11 * * *', async () => {
     // send notifications at 12:00 PM every day
     await notificationService.sendEventsNotifications();
