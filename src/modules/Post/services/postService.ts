c993d07d1f9dc175e4d03816202a264fea97f465
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-17 16:29:46 +0300

    Refactor: Post service

--- src/modules/Post/services/postService.ts
+++ src/modules/Post/services/postService.ts
@@ -64,7 +64,7 @@ export class PostService {
     userId?: number,
   ) {
     return await this.postRepo.postsWithLikesAndComments(
-      { ...options },
+      options,
       postId,
       userId,
     );
