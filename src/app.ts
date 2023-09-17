87cad7e4333f62d316a7c43a84650d5be6423e51
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-17 13:18:44 +0300

    Refactor: jobs file
    - Renamed the function (added 's') to follow naming convention.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import { errorHandlerMiddleware } from './middlewares/';
-import { scheduleDailyNotification } from './helpers';
+import { scheduleDailyNotifications } from './helpers';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use(errorHandlerMiddleware);
-scheduleDailyNotification();
+scheduleDailyNotifications();
 
 export default app;
