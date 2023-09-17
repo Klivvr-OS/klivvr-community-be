5f786f1dc60f96bf403a9ca2bacc2b33e02fe8c7
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-17 13:36:12 +0300

    Refactor: Notification service
    - Imported findUsersDeviceTokens from Device Token service.
    - Enhanced logic and removed redundent code to be more clear while sending notifications.

--- src/modules/Notification/services/notificationService.ts
+++ src/modules/Notification/services/notificationService.ts
@@ -1,6 +1,10 @@
 import { Prisma, Event } from '@prisma/client';
 import { NotificationRepo, notificationRepo } from '../repos/notificationRepo';
-import { eventService, userService, novuService } from '../../../modules';
+import {
+  eventService,
+  deviceTokenService,
+  novuService,
+} from '../../../modules';
 import { getEventNotificationPayload } from './config';
 
 export class NotificationService {
@@ -12,24 +16,24 @@ export class NotificationService {
 
   async sendEventsNotifications() {
     const todayEvents = (await eventService.findTodayEvents()) as Event[];
-    const users = await userService.findUsersDeviceToken();
+    const users = await deviceTokenService.findUsersDeviceToken();
     for (const event of todayEvents) {
       for (const user of users) {
-        const isCurrentUser = event.userId === user.id;
+        const isCurrentUser = event.userId === user.userId;
         const { title, description } = getEventNotificationPayload({
           name: event.name,
           isCurrentUser,
-          eventType: event.eventType,
+          type: event.type,
         });
         await Promise.all([
           novuService.triggerNotification(
             { title, description },
-            user.id.toString(),
+            user.userId.toString(),
           ),
           this.createOne({
             title,
             description,
-            userId: user.id,
+            userId: user.userId,
           }),
         ]);
       }
