c23b5de84a7d5d44192db5c44b6f1fbb5f6b00b5
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-17 13:17:31 +0300

    Refactor: Schema
    - Renamed eventType to type in event table.
    - Formatted the schema.

---
+++ src/database/prisma/migrations/20230917085753_change_event_type_to_type_in_event_table/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `eventType` on the `Event` table. All the data in the column will be lost.
+  - Added the required column `type` to the `Event` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Event" DROP COLUMN "eventType",
+ADD COLUMN     "type" "EventType" NOT NULL;
